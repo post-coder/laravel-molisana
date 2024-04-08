d38651edbd245c33765be5c1176242a1fd6c3651
post-coder <gabrielspanu.dev@gmail.com>
2024-04-08 12:23:52 +0200

    feat: layout di prova completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,29 @@
 
 @section('content')
 
+<main>
+
+    <div class="container">
+        
+        <h2>Le lunghe</h2>
+    
+        <div id="pasta-list" class="row row-cols-3">
+            <div class="pasta">
+                <img class="pasta-img" src="https://www.lamolisana.it/wp-content/uploads/2021/09/4-spaghetto-quadrato-bucato.jpg" alt="">
+                <div class="pasta-title">N.4 Spaghetto Quadrato</div>
+            </div>
+            <div class="pasta">
+                <img class="pasta-img" src="https://www.lamolisana.it/wp-content/uploads/2021/09/4-spaghetto-quadrato-bucato.jpg" alt="">
+                <div class="pasta-title">N.4 Spaghetto Quadrato</div>
+            </div>
+            <div class="pasta">
+                <img class="pasta-img" src="https://www.lamolisana.it/wp-content/uploads/2021/09/4-spaghetto-quadrato-bucato.jpg" alt="">
+                <div class="pasta-title">N.4 Spaghetto Quadrato</div>
+            </div>
+        </div>
+    </div>
+
+
+</main>
+
 @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <img class="logo" src="{{ Vite::asset('resources/img/marchio-sito-test.png') }}" alt="Logo la molisana">
+
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li class="active">Prodotti</li>
+            <li>News</li>
+        </ul>
+    </nav>
+</header>
