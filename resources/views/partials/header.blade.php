5c9040d99babe14e88e3bcbaa2c33550d5240f01
post-coder <gabrielspanu.dev@gmail.com>
2024-04-17 09:10:01 +0200

    feat: navbar dark

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
     <img class="logo" src="{{ Vite::asset('resources/img/marchio-sito-test.png') }}" alt="Logo la molisana">
 
     {{-- navbar --}}
-    <nav>
+    <nav class="nav navbar-dark ">
         <ul>
             <li><a href="{{route('home')}}">Home</a></li>
             <li class="active"><a href="{{route('home')}}">Prodotti</a></li>
